814f187b8ac592710bd7ee252742dd085f90c9be
Huzaifa Zahid <huzaifazahid445@gmail.com>
2026-02-18 17:32:00 +0500

    Update sessions_controller.rb
    added restrictions to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+  before_action :logged_in_redirect, except: [ :destroy ]
   def new
   end
 
@@ -18,5 +19,13 @@ class SessionsController < ApplicationController
     session[:user_id] = nil
     flash[:success] = "Logged out successfully"
     redirect_to login_path
+  end
+
+  private
+  def logged_in_redirect
+    if logged_in?
+      flash[:alert] = "You are already logged in."
+      redirect_to root_path
+    end
   end
 end
